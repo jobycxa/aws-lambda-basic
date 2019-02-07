8e1eb0737d06408fa2228cbeac9387540eac1868
jobycxa <jobycxa@gmail.com>
2019-02-07 11:45:29 +0530

    Updating
    updating with new code

--- echo.py
+++ echo.py
@@ -1,15 +1,12 @@
-from __future__ import print_function
-
 import json
+import datetime
 
-print('Loading function')
 
-
-def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
-
+def handler(event, context):
+    data = {
+        'output': 'Hello World',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
